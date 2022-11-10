38b5c294f86cbbb9daa15a09ed6e9bfd3bb28a51
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2022-11-10 16:52:43 -0500

    Added support for saving contact categories.

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -78,7 +78,7 @@ namespace MyBuddyListPro.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,BirthDate,Address1,Address2,City,USState,ZipCode,Email,PhoneNumber,ImageFile")] Contact contact)
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,BirthDate,Address1,Address2,City,USState,ZipCode,Email,PhoneNumber,ImageFile")] Contact contact, List<int> CategoryList)
         {
             ModelState.Remove("AppUserId");
 
@@ -100,6 +100,15 @@ namespace MyBuddyListPro.Controllers
 
                 _context.Add(contact);
                 await _context.SaveChangesAsync();
+
+                // Loop over all the selected categories.
+                foreach (int categoryId in CategoryList)
+                {
+                    await _addressBookService.AddContactToCategoryAsync(categoryId, contact.Id);
+                }
+
+                // Save each category selected to the categories table.
+
                 return RedirectToAction(nameof(Index));
             }
 
