70a6e79377bbd8fe291f97ed6adcef3fd4108beb
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2022-11-18 10:30:35 -0500

    Implemented a custom error page and error handler.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,25 @@ namespace MyBuddyListPro.Controllers
             return View();
         }
 
+        [Route("/Home/ErrorHandler/{code:int}")]
+        public IActionResult ErrorHandler(int code)
+        {
+            var customError = new CustomError();
+
+            customError.code = code;
+
+            if (code == 404)
+            {
+                customError.message = "Sorry, the page you requested could not be found.";
+            }
+            else
+            {
+                customError.message = "Sorry, an error has occurred.";
+            }
+
+            return View("~/Views/Shared/ErrorPage.cshtml", customError);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Models/CustomError.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBuddyListPro.Models
+{
+    public class CustomError
+    {
+        public int code { get; set; }
+        public string? message { get; set; }
+    }
+}
