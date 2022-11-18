9db7da61c6a7a80e312768e6f783bba88f705612
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2022-11-18 15:09:00 -0500

    Added data helper functionality.

---
+++ Helpers/DataHelper.cs
@@ -0,0 +1,17 @@
+﻿using MyBuddyListPro.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBuddyListPro.Helpers
+{
+    public static class DataHelper
+    {
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            // Get an instance of the database application context.
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+            
+            // Migration: this is equivalent to update-database.
+            await dbContextSvc.Database.MigrateAsync();
+        }
+    }
+}
