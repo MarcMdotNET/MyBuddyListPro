0857d1623eb757f6d6eef7b90b28bd109ab7cf90
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2022-11-20 00:11:30 -0500

    Made small improvements and tweaks.

--- Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -52,7 +52,7 @@ namespace MyBuddyListPro.Areas.Identity.Pages.Account
                 return RedirectToPage("/Index");
             }
             returnUrl = returnUrl ?? Url.Content("~/");
-
+            
             var user = await _userManager.FindByEmailAsync(email);
             if (user == null)
             {
@@ -61,7 +61,7 @@ namespace MyBuddyListPro.Areas.Identity.Pages.Account
 
             Email = email;
             // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
+            DisplayConfirmAccountLink = false;
             if (DisplayConfirmAccountLink)
             {
                 var userId = await _userManager.GetUserIdAsync(user);
